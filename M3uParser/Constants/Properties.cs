e9bbf6474f1a343ed08264f56dc8f35067c9d0ea
AcidFabrik <karolis.arbaciauskas@gmail.com>
2019-11-05 01:19:09 +0200

    apply code formating

--- M3uParser/Constants/Properties.cs
+++ M3uParser/Constants/Properties.cs
@@ -1,35 +1,35 @@
 namespace M3UParser.Constants
 {
-	public static class Properties
-	{
-		/// <summary>
-		/// Channel code from EPG code table.
-		/// </summary>
-		public const string TvgId = "tvg-id";
-		
-		/// <summary>
-		/// Channel name from EPG code table.
-		/// </summary>
-		public const string TvgName = "tvg-name";
-		
-		/// <summary>
-		/// Path to the big channel logo, minimum height should be 48px.
-		/// </summary>
-		public const string TvgLogo = "tvg-logo";
+    public static class Properties
+    {
+        /// <summary>
+        /// Channel code from EPG code table.
+        /// </summary>
+        public const string TvgId = "tvg-id";
 
-		/// <summary>
-		/// Path to the small square channel logo, min height should be 48px (works in the latest app versions only).
-		/// </summary>
-		public const string TvgLogoSmall = "tvg-logo-small";
+        /// <summary>
+        /// Channel name from EPG code table.
+        /// </summary>
+        public const string TvgName = "tvg-name";
 
-		/// <summary>
-		/// Channel group.
-		/// </summary>
-		public const string GroupTitle = "group-title";
-		
-		/// <summary>
-		/// Group parent code.
-		/// </summary>
-		public const string ParentCode = "parent-code";
-	}
+        /// <summary>
+        /// Path to the big channel logo, minimum height should be 48px.
+        /// </summary>
+        public const string TvgLogo = "tvg-logo";
+
+        /// <summary>
+        /// Path to the small square channel logo, min height should be 48px (works in the latest app versions only).
+        /// </summary>
+        public const string TvgLogoSmall = "tvg-logo-small";
+
+        /// <summary>
+        /// Channel group.
+        /// </summary>
+        public const string GroupTitle = "group-title";
+
+        /// <summary>
+        /// Group parent code.
+        /// </summary>
+        public const string ParentCode = "parent-code";
+    }
 }
--- M3uParser/Models/M3uPlaylist.cs
+++ M3uParser/Models/M3uPlaylist.cs
@@ -2,11 +2,11 @@ using System.Collections.Generic;
 
 namespace M3UParser.Models
 {
-	public class M3uPlaylist
-	{
-		/// <summary>
-		/// List of playlist entries.
-		/// </summary>
-		public List<M3uPlaylistEntry> PlaylistEntries { get; }
-	}
+    public class M3uPlaylist
+    {
+        /// <summary>
+        /// List of playlist entries.
+        /// </summary>
+        public List<M3uPlaylistEntry> PlaylistEntries { get; }
+    }
 }
