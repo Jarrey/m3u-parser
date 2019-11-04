e626dcb4dd7e4b126953b8d63eb0a03f4fae5d23
AcidFabrik <karolis.arbaciauskas@gmail.com>
2019-11-05 01:16:26 +0200

    apply code formating

--- M3uParser/Utils/Utils.cs
+++ M3uParser/Utils/Utils.cs
@@ -2,36 +2,36 @@ using System;
 
 namespace M3UParser.Utils
 {
-	public static class Helpers
-	{
-		public static string GetTitle(string line)
-		{
-			if (string.IsNullOrWhiteSpace(line)) throw new ArgumentException("Text input could not be empty or null.");
-			var titleIndex = line.IndexOf(',');
-			if (titleIndex == -1) throw new Exception($"Failed to find title in string: {line}");
+  public static class Helpers
+  {
+    public static string GetTitle(string line)
+    {
+      if (string.IsNullOrWhiteSpace(line)) throw new ArgumentException("Text input could not be empty or null.");
+      var titleIndex = line.IndexOf(',');
+      if (titleIndex == -1) throw new Exception($"Failed to find title in string: {line}");
 
-			return line.Substring(titleIndex + 1);
-		}
+      return line.Substring(titleIndex + 1);
+    }
 
-		public static string GetProperties(string line, string property)
-		{
-			if (string.IsNullOrWhiteSpace(line)) throw new ArgumentException("Text input could not be empty or null.");
+    public static string GetProperties(string line, string property)
+    {
+      if (string.IsNullOrWhiteSpace(line)) throw new ArgumentException("Text input could not be empty or null.");
 
-			try
-			{
-				if (!line.Contains(property))
-					throw new ArgumentException(
-						$"Property {property} not found in line: {line}.");
+      try
+      {
+        if (!line.Contains(property))
+          throw new ArgumentException(
+            $"Property {property} not found in line: {line}.");
 
-				var propertyStartIndex = line.Substring(line.IndexOf(property) + property.Length + 2);
-				var propertyLastIndex = propertyStartIndex.IndexOf("\"");
+        var propertyStartIndex = line.Substring(line.IndexOf(property) + property.Length + 2);
+        var propertyLastIndex = propertyStartIndex.IndexOf("\"");
 
-				return propertyStartIndex.Substring(0, propertyLastIndex);
-			}
-			catch
-			{
-				return null;
-			}
-		}
-	}
+        return propertyStartIndex.Substring(0, propertyLastIndex);
+      }
+      catch
+      {
+        return null;
+      }
+    }
+  }
 }
